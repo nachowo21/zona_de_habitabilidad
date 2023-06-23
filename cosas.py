81c7d7e79879f59a9597670406b1f4b5d72d220e
nachowo <ignacio.solis.m@usach.cl>
2023-06-23 19:07:24 -0400

    ADD - agregue el archivo cosas.py

---
+++ cosas.py
@@ -0,0 +1,13 @@
+x = input('intruduce algo : ')
+print (x)
+
+
+# git add (stage area)
+# git commit -m "ADD - se indica el cambio del codigo o otra cosa, en este caso agregue el archivo cosas.py"
+# git push
+
+
+## esta parte de aca es para la nomenclatura de los usuarios de git
+# ADD - archivos o funcionalidades nuevas
+# CHANGE - indicar que cambios se han hecho
+# FIX - sirve para indicar el bug que se está corrigiendo 
